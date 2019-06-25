4b867ab263888d955b5fe14a2c10591ff8791240
zhihuidemimi <994903808@qq.com>
2019-06-26 00:19:53 +0800

    helloworld

---
+++ main.go
@@ -0,0 +1,16 @@
+// helloworld project main.go
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("please enter your password:")
+	var password string
+	//fmt.Scanf("%s", &password)
+	fmt.scan("*", &password)
+	fmt.Printf("your password is: " + password)
+	os.Exit(0)
+}
